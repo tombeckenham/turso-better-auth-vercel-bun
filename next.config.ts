c7843844addd0b361bb211232129e0672db55d03
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-20 10:33:22 +1100

    Added ws to see if that works

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["ws"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ignore ws module during bundling
-      config.resolve = config.resolve || {};
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        ws: false,
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;

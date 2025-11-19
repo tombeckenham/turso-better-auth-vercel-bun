6ccac2460a4d232490620a77dd2d23c9f81cdad6
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-20 06:31:22 +1100

    Added auth routes

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import SignIn from "@/components/sign-in";
+
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background px-4">
+      <SignIn />
+    </div>
+  );
+}
+
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import SignUp from "@/components/sign-up";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background px-4">
+      <SignUp />
+    </div>
+  );
+}
+

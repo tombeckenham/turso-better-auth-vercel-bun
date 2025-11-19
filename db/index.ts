c5bb99ac7a715f363d2862a3215fefb66f28e028
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-20 07:32:28 +1100

    Changed to http on libsql

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle } from "drizzle-orm/libsql/http";
 import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 

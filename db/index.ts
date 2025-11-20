abf275f8b341e0b5f8b4e5e5866bde8ae7d91d9d
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-20 11:37:19 +1100

    Trying the new beta drizzle

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,13 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@tursodatabase/serverless/compat";
-import { type Client } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql/http";
 
 import * as schema from "./schema";
 
-const client = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+  schema,
 });
-
-const db = drizzle({ client: client as unknown as Client, schema });
 
 export { db };

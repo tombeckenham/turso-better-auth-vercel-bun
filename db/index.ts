90b6d6cbc6e287e1e260f739f5b7a79f5fa3cda5
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-20 12:25:02 +1100

    Reverted back to normal non-beta drizzle

--- db/index.ts
+++ db/index.ts
@@ -1,13 +1,13 @@
-import { drizzle } from "drizzle-orm/libsql/http";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
 
 import * as schema from "./schema";
 
-const db = drizzle({
-  connection: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  },
-  schema,
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
 });
+
+const db = drizzle({ client, schema: { ...schema } });
 
 export { db };

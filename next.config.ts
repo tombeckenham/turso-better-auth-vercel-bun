1fdef0852637f6a80a83c9bc72194054c74651c0
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2025-11-20 11:56:57 +1100

    Added ws to the external dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["@libsql/isomorphic-ws"],
+  serverExternalPackages: ["@libsql/isomorphic-ws", "ws"],
 };
 
 export default nextConfig;
